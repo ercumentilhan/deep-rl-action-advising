bccdb903fe9a61871beea27a3165e3ac9635bafc
Ercument Ilhan <ercumentilhan@gmail.com>
2021-11-05 12:34:10 +0000

    Major overhaul

--- code/plot_generator.py
+++ code/plot_generator.py
@@ -16,6 +16,17 @@ import seaborn as sns
 sns.set()
 sns.set_style("darkgrid", {"axes.facecolor": ".9"})
 
+RUNS_DIR = 'E:\\Runs'
+GAME_DIR = 'Seaquest'
+
+TEACHER_SCORE = 8178.0
+
+TAGS = [
+    'Evaluation/Reward_Real',
+    'Evaluation_B/Reward_Real',
+    'Advices_Taken',
+    'Advices_Reused/All'
+]
 
 # ======================================================================================================================
 
@@ -41,24 +52,23 @@ def walklevel(some_dir, level=1):
 
 # ======================================================================================================================
 
-def export_to_csv(input_dir, output_dir, requested_tags):
-    print(input_dir, output_dir, requested_tags)
+def export_to_csv(input_dir, output_dir, requested_tag):
+    print(input_dir, output_dir, requested_tag)
     summary_iterator = EventAccumulator(input_dir).Reload()
     tags = summary_iterator.Tags()['scalars']
     data = defaultdict(list)
-    steps = []
-    for tag in tags:
-        if tag in requested_tags:
-            steps = [e.step for e in summary_iterator.Scalars(tag)]
-            event = summary_iterator.Scalars(tag)
-            data[tag] = [e.value for e in event]
 
-    tags, values = zip(*data.items())
-    np_values = np.array(values)
-    for index, tag in enumerate(tags):
-        tag = tag.replace("/", "-")
-        df = pd.DataFrame(np_values[index], index=steps)
-        df.to_csv(os.path.join(output_dir, tag + '.csv'), header=False)
+    if requested_tag in tags:
+        steps = [e.step for e in summary_iterator.Scalars(requested_tag)]
+        event = summary_iterator.Scalars(requested_tag)
+        data[requested_tag] = [e.value for e in event]
+
+        tags, values = zip(*data.items())
+        np_values = np.array(values)
+        for index, tag in enumerate(tags):
+            tag = tag.replace("/", "-")
+            df = pd.DataFrame(np_values[index], index=steps)
+            df.to_csv(os.path.join(output_dir, tag + '.csv'), header=False)
 
 # ======================================================================================================================
 
@@ -82,7 +92,7 @@ def generate_csv_files(summaries_dir, requested_tags):
             for requested_tag in requested_tags:
                 tag = [requested_tag,]
                 if not os.path.exists(seed_dir + '/' + tag[0].replace("/", "-") + '.csv'):
-                    export_to_csv(seed_dir, seed_dir, tag)
+                    export_to_csv(seed_dir, seed_dir, tag[0])
 
 # ======================================================================================================================
 
@@ -141,7 +151,13 @@ def generate_plots(summaries_dir, plots_dir, tag):
 def generate_combined_plot(summaries_dir, plots_dir, tag):
     os.makedirs(plots_dir, exist_ok=True)
 
-    span = 5  # Plot smoothing span
+    # Plot smoothing span
+    if tag == 'Advices_Taken':
+        span = 30
+    elif tag == 'Advices_Reused/All':
+        span = 150
+    else:
+        span = 5
 
     pda_all, labels = [], []
     run_dirs, plot_dirs = [], []
@@ -155,32 +171,81 @@ def generate_combined_plot(summaries_dir, plots_dir, tag):
             seed_dirs.append(os.path.join(run_dir, d))
 
         plot_data_x, plot_data_y = [], []
+        pds_x, pds_y = [], []
+        lengths = []
 
         for seed_dir in seed_dirs:
-            pd_x, pd_y = read_data(os.path.join(seed_dir, tag.replace("/", "-") + '.csv'))
-            plot_data_x.append(pd_x)
-            plot_data_y.append(pd_y)
+            csv_filepath = os.path.join(seed_dir, tag.replace("/", "-") + '.csv')
+            if os.path.isfile(csv_filepath):
+                pd_x, pd_y = read_data(csv_filepath)
+                pds_x.append(pd_x)
+                pds_y.append(pd_y)
+                lengths.append(np.shape(pd_x)[0])
 
-        pda = pd.DataFrame(plot_data_y, columns=plot_data_x[0])
-        pda = pda.ewm(axis=1, span=span).mean()
-        pda = pda.melt()
+        if len(pds_x) > 0:
+            min_length = min(lengths)
+            for i in range(len(pds_x)):
+                pd_x = pds_x[i][:min_length, ]
+                pd_y = pds_y[i][:min_length, ]
 
-        pda_all.append(pda)
+                plot_data_x.append(pd_x)
+                plot_data_y.append(pd_y)
 
-    fig, ax = plt.subplots(figsize=(12, 8), dpi=100, )
+            print(run_dir, tag)
 
-    for i, _ in enumerate(run_dirs):
-        sns.lineplot(x='variable', y='value', data=pda_all[i], legend='brief', err_style='band',
-                     label=labels[i], ci='sd')
+            pda = pd.DataFrame(plot_data_y, columns=plot_data_x[0])
+            pda = pda.ewm(axis=1, span=span).mean()
+            pda = pda.melt()
 
-    ax.set_ylim([-200, None])
+            pda_all.append(pda)
+        else:
+            pda_all.append(None)
 
-    # Adjust legend
+    if tag == 'Advices_Taken':
+        fig, ax = plt.subplots(figsize=(12, 4), dpi=150, )
+    else:
+        fig, ax = plt.subplots(figsize=(12, 8), dpi=100, )
+
+    for i, _ in enumerate(pda_all):
+        if pda_all[i] is not None:
+            sns.lineplot(x='variable', y='value', data=pda_all[i], legend='brief', err_style='band',
+                         label=labels[i], ci='sd', linewidth=1)
+
+    if tag == 'Evaluation/Reward_Real' or tag == 'Evaluation_B/Reward_Real':
+        plt.axhline(y=TEACHER_SCORE, color='slategray', linestyle='--')
+
+    x_lim, y_lim = [None, None], [None, None]
+
+    if tag == 'Advices_Taken' or \
+            tag == 'Advices_Reused/All':
+        y_lim = [-5, None]
+        x_lim = [-10, None]
+
+        modify_and_save_plot(ax, tag, x_lim, y_lim, tag.replace("/", "-"))
+
+        if tag == 'Advices_Taken':
+            x_lim = [-10, 250000]
+            modify_and_save_plot(ax, tag, x_lim, y_lim, tag.replace("/", "-") + '_Zoomed')
+    else:
+        y_lim = [-200, None]
+        modify_and_save_plot(ax, tag, x_lim, y_lim, tag.replace("/", "-"))
+
+# ======================================================================================================================
+
+def modify_and_save_plot(ax, tag, x_lim, y_lim, filename):
+    ax.set_xlim(x_lim)
+    ax.set_ylim(y_lim)
     plt.setp(ax.get_legend().get_texts(), fontsize='20')
     plt.setp(ax.get_legend().get_title(), fontsize='25')
 
     ax_handles, ax_labels = ax.get_legend_handles_labels()
-    ax.legend(ax_handles, ax_labels, loc='lower right', labelspacing=0.25)
+
+    print(ax_handles, ax_labels)
+
+    legend_loc = 'upper left'
+    if tag == 'Advices_Taken':
+        legend_loc = 'upper right'
+    ax.legend(ax_handles, ax_labels, loc=legend_loc, labelspacing=0.25)
 
     plt.tight_layout()
     fig = ax.get_figure()
@@ -188,31 +253,22 @@ def generate_combined_plot(summaries_dir, plots_dir, tag):
     ax.xaxis.label.set_visible(False)
     ax.yaxis.label.set_visible(False)
 
-    fig.savefig(os.path.join(plots_dir, tag.replace("/", "-") + '.png'))
+    fig.savefig(os.path.join(plots_dir, filename + '.png'))
 
-    fig.clear()
-    plt.close(fig)
+    #fig.clear()
+    #plt.close(fig)
 
 # ======================================================================================================================
 
-
-runs_dir = 'E:/Runs/Summaries/LunarLander'
-game_dir = 'B25000'
-summaries_dir = os.path.join(runs_dir, game_dir)
+summaries_dir = os.path.join(RUNS_DIR, GAME_DIR)
 
 if os.path.isdir(summaries_dir) and len(os.listdir(summaries_dir)) != 0:
-    plots_dir = os.path.join(runs_dir, game_dir + '_Plots')
-    tags = [
-        'Reward_Real/Steps',
-        'Evaluation/Reward_Real',
-        'Advices_Taken'
-            ]
+    plots_dir = os.path.join(RUNS_DIR, GAME_DIR + '_Plots')
 
     if os.path.exists(plots_dir):
         shutil.rmtree(plots_dir)
 
-    generate_csv_files(summaries_dir, tags)
+    # generate_csv_files(summaries_dir, TAGS)
 
-    for tag in tags:
-        generate_plots(summaries_dir, plots_dir, tag)
+    for tag in TAGS:
         generate_combined_plot(summaries_dir, plots_dir, tag)
